37aa2338ee8d2d78224b3c12079fe13c35a07da8
anthony974 <t-a-kondo@hotmail.fr>
2013-11-01 16:35:06 +0100

    Classe qui gère le tchat de la map!

---
+++ OptimusApi/Bot/Game/Tchat/BotMessenger.cs
@@ -0,0 +1,54 @@
+﻿using NLog;
+using Optimus.Common.Dispatching;
+using Optimus.Common.Enums;
+using Optimus.Common.Protocol.Messages;
+using OptimusApi.Bot.Interface.Tchat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// class non terminé!
+namespace OptimusApi.Bot.Game.Tchat
+{
+    public delegate void MessageReceived(object sender, IMessenger message);
+    public class BotMessenger
+    {
+        private BotManager bot;
+        public event MessageReceived MessageReceived;
+        public BotMessenger(BotManager client)
+        {
+            bot = client;
+            bot.Network.Dispatcher.Register(this);
+        }
+
+        public void SendMessage(string message,  ChannelTchatEnum channel = ChannelTchatEnum.CHANNEL_GLOBAL)
+        {
+            ChatClientMultiMessage CCMM = new ChatClientMultiMessage(message, (sbyte)channel);
+            bot.Network.Send(CCMM);
+        }
+
+        public void SendPrivateMessage(string message, string target)
+        {
+            ChatClientPrivateMessage ccpm = new ChatClientPrivateMessage(message, target);
+            bot.Network.Send(ccpm);
+        }
+
+        [MessageHandler(TextInformationMessage.Id, PriorityPacket.VERY_HIGH)]
+        private void HandlerTextInformationMessage(TextInformationMessage message)
+        {
+
+        }
+
+        [MessageHandler(ChatServerMessage.Id, PriorityPacket.VERY_HIGH)]
+        private void HandlerServerMessage(ChatServerMessage message)
+        {
+            IMessenger packet = new Messenger(message.senderName, message.content, (ChannelTchatEnum)(int)message.channel);
+            if (this.MessageReceived != null)
+            {
+                    MessageReceived(this, packet);
+            }
+        }
+    }
+}
