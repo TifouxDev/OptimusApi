89a53b3709e713130917e73feb6da448a10e4db8
anthony974 <t-a-kondo@hotmail.fr>
2013-11-03 18:07:35 +0100

    Cette class permet de montré un exemple de l'utilisation de l'évenement BotGameConnected! et aussi mes premet essais sur mon pathfinding

--- BotApiTest/test.cs
+++ BotApiTest/test.cs
@@ -1,6 +1,9 @@
 ﻿using Optimus.Common.Dispatching;
 using Optimus.Common.Protocol.Messages;
 using OptimusApi.Bot;
+using OptimusApi.Bot.Game.Tchat;
+using OptimusApi.Tool.D2P;
+using OptimusApi.Tool.D2P.Map;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,13 +13,19 @@ using System.Threading.Tasks;
 
 namespace BotApiTest
 {
-    class test
+    public class test
     {
         private BotManager bot; 
         public test(BotManager client)
         {
             bot = client;
+            bot.BotGameConnected += new BotConnectedGame(Connected);
+        }
+
+        private void Connected(object sender)
+        {
             bot.Network.Dispatcher.Register(this);
+            bot.Game.World.Tchat.MessageReceived += new MessageReceived(Tchat_MessageReceived);
         }
 
         [MessageHandler(MapComplementaryInformationsDataMessage.Id)]
@@ -28,9 +37,25 @@ namespace BotApiTest
         //test tchat
         private void SendTest()
         {
-            bot.Game.World.Tchat.SendMessage("Salut", Optimus.Common.Enums.ChannelTchatEnum.CHANNEL_GLOBAL);
+         //  Thread.Sleep(500);
+           // bot.Game.World.Tchat.SendMessage("sa m'enerve", Optimus.Common.Enums.ChannelTchatEnum.CHANNEL_GLOBAL);
             Thread.Sleep(500);
-            bot.Game.World.Tchat.SendPrivateMessage("Salut copain", "Baalis");
+            MapManager map  = new MapManager(@"C:\Program Files (x86)\Dofus2\app\content\maps");
+            Map currentMap = map.GetMap(bot.Game.World.MapID);
+            Pathfinding.Pathfinding pathfinder = new Pathfinding.Pathfinding(bot.Game.Character.CellId, 326, currentMap);
+            GameMapMovementRequestMessage gmmrm = new GameMapMovementRequestMessage(pathfinder.GetPath().ToArray(), bot.Game.World.MapID);
+            bot.Network.Send(gmmrm);
+            if(pathfinder.LenghtPath <= 2){
+                Thread.Sleep(450 * pathfinder.LenghtPath);
+            }else{
+                      Thread.Sleep(200 * pathfinder.LenghtPath);
+            }
+            bot.Network.Send(new GameMapMovementConfirmMessage());
+        }
+
+        private void Tchat_MessageReceived(object sender, OptimusApi.Bot.Interface.Tchat.IMessenger message)
+        {
+            Console.WriteLine(string.Format("[{0}] {1} : {2}", message.Channel.ToString(), message.Author, message.Message));
         }
     }
 }
